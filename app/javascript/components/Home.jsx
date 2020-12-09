f4668024c0e6c76372ea981907ea467f5af42637
Robert Wise <robcodehub@gmail.com>
2020-12-09 17:21:19 +1100

    minor change to h1 tag on home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const HomeDiv = styled.div`
+  padding-top: 4rem;
+  margin-top: 4rem;
+`;
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-    </div>
-  )
+    <HomeDiv>
+      <h1>Home Page</h1>
+    </HomeDiv>
+  );
 }
