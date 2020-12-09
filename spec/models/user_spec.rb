ac2c38f4da2d6f814f78cfc2098f4ea41bc1600a
Robert Wise <robcodehub@gmail.com>
2020-12-09 11:26:12 +1100

    updated rspec tests and checked all tests passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,41 +1,26 @@
-
 class User
-  attr_accessor :first_name
-  attr_accessor :last_name
-  attr_accessor :email
-
+  attr_reader :first_name
+  attr_reader :last_name
+  attr_reader :email
   def initialize(first_name, last_name, email)
-    @first_name = niche_type
+    @first_name = first_name
     @last_name = last_name
     @email = email
   end
 end
 
-
-RSpec.describe User, type: :model do
-  context 'validation tests' do
-    it 'ensures that a first name is entered' do
-      user = User.new(last_name: 'LastName', email: 'test@test.com').save
-      expect(user).to eq(false)
+RSpec.describe User do
+  let(:user) {described_class.new('Rob', 'Test', 'test@test.com')}
+    it 'ensures that an email is entered' do
+      expect(user.email).to eq('test@test.com')
     end
 
     it 'ensures that a last name is entered' do
-      user = User.new(first_name: 'FirstName', email: 'test@test.com').save
-      expect(user).to eq(false)
+      expect(user.last_name).not_to eq(nil)
     end
 
     it 'ensures that an email address is entered' do
-      user = User.new(first_name: 'FirstName', last_name: 'LastName').save
-      expect(user).to eq(false)
+      expect(user.email).not_to eq(nil)
     end
 
-    it 'should save successfully' do
-      user = User.new(first_name: 'FirstName', last_name: 'LastName', email: 'test@test.com').save
-      expect(user).to eq(true)
-    end
-
-  end
-
-  context 'scope tests' do
-  end
 end
