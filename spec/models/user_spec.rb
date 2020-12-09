0f7daf05cdd5c83d90cf27aefc3150f445c8274d
Robert Wise <robcodehub@gmail.com>
2020-12-09 16:46:52 +1100

    updated RSpec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,18 @@ class User
   attr_reader :first_name
   attr_reader :last_name
   attr_reader :email
-  def initialize(first_name, last_name, email)
+  attr_reader :saved_listings
+
+  def initialize(first_name, last_name, email, saved_listings)
     @first_name = first_name
     @last_name = last_name
     @email = email
+    @saved_listings = saved_listings
   end
 end
 
 RSpec.describe User do
-  let(:user) {described_class.new('Rob', 'Test', 'test@test.com')}
+  let(:user) {described_class.new('Rob', 'Test', 'test@test.com', 0)}
     it 'ensures that an email is entered' do
       expect(user.email).to eq('test@test.com')
     end
@@ -23,4 +26,11 @@ RSpec.describe User do
       expect(user.email).not_to eq(nil)
     end
 
+    it 'expects username to be Rob' do
+        expect(user.first_name).to eq('Rob')
+      end
+
+      it 'user has no saved listings' do
+        expect(user.saved_listings).to eq(0)
+      end
 end
